9eace20e9e44530d33137cf6d33b6842fea3f04a
Mary <mary.deroost@gmail.com>
2020-09-10 11:49:58 +0200

    Added new unit test

---
+++ tests/unit/dataprocessing/test_time_dataprocessing.py
@@ -0,0 +1,13 @@
+import pytest
+import pandas as pd
+
+from movies.dataprocessing.time_dataprocessing import format_time_to_minutes
+
+
+@pytest.mark.unit_test
+def test_format_time_to_minutes():
+
+    df = pd.DataFrame({"duration": ["1h 45min", "30min", "2h"]})
+    expected = [105, 30, 120]
+
+    assert format_time_to_minutes(df, "duration")["duration"].tolist() == expected
