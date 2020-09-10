6b3a4cc7fb27f16649de9aeff0e67b056df46d6e
Mary <mary.deroost@gmail.com>
2020-09-10 11:09:30 +0200

    Renamed file and added two functions

---
+++ src/movies/dataprocessing/dataprocessing_money.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from utils import CURRENCY_VALUES
+
+
+def determine_currencies(df: pd.DataFrame, money_column: str) -> pd.DataFrame:
+
+    df["currency"] = df[money_column].str.extract("([a-zA-Z\$]+)")
+
+    df["currency_value"] = df["currency"].map(CURRENCY_VALUES).astype(float)
+
+    return df
+
+
+def convert_money_columns(df: pd.DataFrame, money_column: str) -> pd.DataFrame:
+
+    df[money_column] = df[money_column].str.replace(",", "")
+    df = determine_currencies(df, money_column)
+
+    df[money_column] = df[money_column].str.extract("(\d+)").astype(float)
+    df[money_column] = df[money_column] * df["currency_value"]
+
+    return df
