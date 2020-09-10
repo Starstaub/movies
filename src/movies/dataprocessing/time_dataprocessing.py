542369ffab5743eb982e4d65c424115671e227d5
Mary <mary.deroost@gmail.com>
2020-09-10 11:49:52 +0200

    Added new function

---
+++ src/movies/dataprocessing/time_dataprocessing.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from datetime import datetime
+
+
+def format_time_to_minutes(df: pd.DataFrame, time_column: str) -> pd.DataFrame:
+
+    df[time_column] = [
+        x + " 0min" if "min" not in x else x for x in df[time_column].values
+    ]
+    df[time_column] = ["0h " + x if "h" not in x else x for x in df[time_column].values]
+    df[time_column] = [
+        datetime.strptime(x, "%Hh %Mmin") for x in df[time_column].values
+    ]
+    df[time_column] = df[time_column].dt.hour * 60 + df[time_column].dt.minute
+
+    return df
