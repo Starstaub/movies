98b410ec3188806a8e68ebbe2c747f997a8c9a9f
Mary <mary.deroost@gmail.com>
2020-09-13 10:43:01 +0200

    Added files with new name and new functions

---
+++ src/movies/dataprocessing/movie_dataprocessing.py
@@ -0,0 +1,50 @@
+from movies.utils import TYPE_DATA
+
+import pandas as pd
+import numpy as np
+
+
+def define_types(value: str) -> str:
+
+    val = "Other"
+
+    for key in TYPE_DATA:
+        if key in value:
+            val = TYPE_DATA[key]
+            break
+
+    return val
+
+
+def clean_release_column(df: pd.DataFrame, column: str) -> pd.DataFrame:
+
+    df[column] = (
+        df[column]
+        .str.replace("TV Mini-Series ", "")
+        .str.replace("TV Series ", "")
+        .str.replace("TV Movie ", "")
+        .str.replace("Video game released ", "")
+    )
+    df[column] = [
+        x.replace("(", "").replace(")", "") if x.startswith("(") else x
+        for x in df[column].values
+    ]
+
+    return df
+
+
+def get_movie_kind(main_df: pd.DataFrame, genre_df: pd.DataFrame) -> pd.DataFrame:
+
+    cols = [x for x in genre_df.columns if "genres" in x]
+
+    conditions = [
+        (genre_df[cols] == "Short").any(axis="columns"),
+        (genre_df[cols] == "Documentary").any(axis="columns"),
+        (genre_df[cols] == "Animation").any(axis="columns"),
+    ]
+
+    choices = ["Short movie", "Documentary", "Animated movie"]
+
+    main_df["kind"] = np.select(conditions, choices, default="Other")
+
+    return main_df
---
+++ tests/unit/dataprocessing/test_movie_dataprocessing.py
@@ -0,0 +1,22 @@
+import pytest
+
+from movies.dataprocessing.movie_dataprocessing import (
+    define_types,
+    clean_release_column,
+)
+
+
+@pytest.mark.unit_test
+def test_define_types(release_data):
+
+    expected = ["TV Series", "Video game", "Other", "TV Mini-Series"]
+
+    assert release_data["release"].apply(define_types).tolist() == expected
+
+
+@pytest.mark.unit_test
+def test_clean_release_column(release_data):
+
+    expected = ["1970-1980", "July 10, 2018", "10 December 2019", "2017"]
+
+    assert clean_release_column(release_data, "release")["release"].tolist() == expected
