5e1dd23606907dcd22146c7845e79554e420cf98
Mary <mary.deroost@gmail.com>
2021-01-15 10:31:52 +0100

    Added new forms, routes and models for user login

--- src/app/forms.py
+++ src/app/forms.py
@@ -1,7 +1,8 @@
-from wtforms import StringField, SelectField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SelectField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from flask_wtf import FlaskForm
 
+from app.models import User
 from utils import FIELDS, COLUMNS, ORDER
 
 
@@ -13,3 +14,30 @@ class MovieSearchForm(FlaskForm):
         " ordered by ", choices=COLUMNS, validators=[DataRequired()]
     )
     chosen_order = SelectField("", choices=ORDER, validators=[DataRequired()])
+
+
+class LoginForm(FlaskForm):
+
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
+
+
+class RegistrationForm(FlaskForm):
+
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password2 = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Sign In")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError(f"'{username.data}' is already used. Please choose a different username.")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError(f"'{email.data}' is already used. Please choose a different e-mail.")
