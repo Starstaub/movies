c52041b5b6cdc02592b02325622c456a1fbfa3a4
Mary <mary.deroost@gmail.com>
2021-01-15 13:17:55 +0100

    Changed title

--- src/app/forms.py
+++ src/app/forms.py
@@ -30,7 +30,7 @@ class RegistrationForm(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     password2 = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo("password")])
-    submit = SubmitField("Sign In")
+    submit = SubmitField("Register")
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
--- src/app/routes.py
+++ src/app/routes.py
@@ -214,7 +214,7 @@ def register():
         db.session.commit()
         flash("Congratulations, you are now a registered user!")
         return redirect(url_for('login'))
-    return render_template("register.html", title="Register", form=form)
+    return render_template("register.html", title="Create your account", form=form)
 
 
 @app.before_request
