a2303496ae031886c01c601efeaaa9f4aa3695c6
Mary <mary.deroost@gmail.com>
2021-01-15 12:33:48 +0100

    Fixed typos and errors

--- src/app/models.py
+++ src/app/models.py
@@ -38,7 +38,7 @@ class Movies(db.Model):
 
 class User(UserMixin, db.Model):
 
-    index = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(60), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
--- src/app/routes.py
+++ src/app/routes.py
@@ -182,13 +182,14 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user is None and not user.check_password(form.password.data):
+        if user is None or not user.check_password(form.password.data):
             flash("Invalid username or password.")
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get("next")
         if not next_page or url_parse(next_page).netloc != "":
             next_page = url_for("index")
+            flash(f"Welcome back {user.username}!")
         return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
 
@@ -196,6 +197,7 @@ def login():
 @app.route("/logout")
 def logout():
     logout_user()
+    flash("You have been successfully logged out. Until next time!")
     return redirect(url_for("index"))
 
 
@@ -206,7 +208,7 @@ def register():
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username_data, email=form.email.data)
+        user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
